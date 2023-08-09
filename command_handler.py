583b19034f1f346c6602a951eac1b6dd3682e27f
jotav <joaovictorgn2004@gmail.com>
2023-08-08 21:46:35 -0300

    command_handler: started undo system

--- command_handler.py
+++ command_handler.py
@@ -9,6 +9,9 @@ def format_command(line):
     command_name = command_args.pop(0)
     return {"name": command_name,"args": command_args}
 
+def save_working_image(response):
+    edit.save_image(response["image"], f'work_img_{response["index"]}', working_image_path, "png")
+
 def handle_command(line, img):
     command = format_command(line)
 
@@ -22,94 +25,96 @@ def handle_command(line, img):
     if hasArgs and args[0] == "test_img":
         args[0] = "test_picture.jpg"
 
-    response = {"image": img,
+    response = {"image": img["image"],
                 "sucsses": False,
                 "hasMessage": False,
-                "message": ""}
+                "message": "",
+                "index": img["index"] + 1,
+                "command": ""}
     
     if command_name == "open":
         response["sucsses"] = True
         response["image"] = edit.open_image(f"{input_path}/{args[0]}")
     
-        edit.save_image(response["image"], "work_img", working_image_path, "png")
+        save_working_image(response)
 
     elif command_name == "show":
-        edit.show_image(img)
+        edit.show_image(img["image"])
         response["sucsses"] = True
 
     elif command_name == "save":
         if hasArgs:
-            edit.save_image(img, args[0], output_path, "png")
+            edit.save_image(img["image"], args[0], output_path, "png")
             response["message"] = f"IMAGE {args[0]} SAVED.\nPATH: {output_path}/{args[0]}.png"
         else:
-            edit.save_image(img, "final_image", output_path, "png")
+            edit.save_image(img["image"], "final_image", output_path, "png")
             response["message"] = f"IMAGE final_image SAVED.\nPATH: {output_path}/{args[0]}.png"
     
         response["sucsses"] = True
         response["hasMessage"] = True
-        response["image"] = img
+        response["image"] = img["image"]
 
     elif command_name == "flip":
         if hasArgs:
             if args[0] == "h":
-                response["image"] = edit.flip_horizontal(img)
+                response["image"] = edit.flip_horizontal(img["image"])
                 response["sucsses"] = True
             elif args[0] == "v":
-                response["image"] = edit.flip_vertical(img)
+                response["image"] = edit.flip_vertical(img["image"])
                 response["sucsses"] = True
             else:
                 response["sucsses"] = False
                 response["hasMessage"] = True
                 response["message"] = f"{args[0]} ISN'T A VALID ARGUMENT !"
-                response["image"] = img
+                response["image"] = img["image"]
         else:
-            response["image"] = edit.flip_horizontal(img)
+            response["image"] = edit.flip_horizontal(img["image"])
             response["sucsses"] = True
 
-        edit.save_image(response["image"], "work_img", working_image_path, "png")
+        save_working_image(response)
     
     elif command_name == "free_resize":
         if hasArgs:
             for i in range(2):
                 if args[i] == "-":
-                    args[i] = edit.image_size(img)[i]
+                    args[i] = edit.image_size(img["image"])[i]
 
             new_size = (int(args[0]), int(args[1]))
             try:
-                response["image"] = edit.free_resize(img, new_size)
+                response["image"] = edit.free_resize(img["image"], new_size)
                 response["sucsses"] = True
 
-                edit.save_image(response["image"], "work_img", working_image_path, "png")
+                save_working_image(response)
             except:
                 response["sucsses"] = False
                 response["hasMessage"] = True
                 response["message"] = f"{new_size} ISN'T A VALID SIZE !"
-                response["image"] = img
+                response["image"] = img["image"]
                 
         else:
             response["sucsses"] = False
             response["hasMessage"] = True
             response["message"] = "THIS FUNCTION NEEDS A SIZE (width and height) !"
-            response["image"] = img
+            response["image"] = img["image"]
 
     elif command_name == "resize":
         if hasArgs:
             try:
-                response["image"] = edit.scale_resize(img, float(args[0]))
+                response["image"] = edit.scale_resize(img["image"], float(args[0]))
                 response["sucsses"] = True
 
-                edit.save_image(response["image"], "work_img", working_image_path, "png")
+                save_working_image(response)
             except:
                 response["sucsses"] = False
                 response["hasMessage"] = True
                 response["message"] = f"{new_size} ISN'T A VALID SCALE !"
-                response["image"] = img
+                response["image"] = img["image"]
         
         else:
             response["sucsses"] = False
             response["hasMessage"] = True
             response["message"] = "THIS FUNCTION NEEDS A SIZE (width and height) !"
-            response["image"] = img
+            response["image"] = img["image"]
 
     elif command_name == "test":
         print("bip bop")
@@ -122,4 +127,4 @@ def handle_command(line, img):
     return response
 
 #TODO: enable edit only when theres a image opened
-#TODO: redo system
+#TODO: undo system // in work
